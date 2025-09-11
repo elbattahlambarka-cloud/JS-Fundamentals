0851af5372fb3c00c352dd6b6a3e5d19fe631f35
Lambarka <elbattahlambarka@gmail.com>
2025-09-11 16:49:52 +0100

    scritp that prints a square

---
+++ 8-square.js
@@ -0,0 +1,15 @@
+if (process.argv[2] === undefined) {
+    console.log('Missing size');
+} else {
+    const size = parseInt(process.argv[2], 10);
+    
+    if (!isNaN(size) && size > 0) {
+        for (let i = 0; i < size; i++) {
+            let row = '';
+            for (let j = 0; j < size; j++) {
+                row += 'X';
+            }
+            console.log(row);
+        }
+    }
+}
