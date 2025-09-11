95d1a310530ba71c847d68e4142e833071259503
Lambarka <elbattahlambarka@gmail.com>
2025-09-11 16:22:14 +0100

    print argument converted to int

---
+++ 5-to_integer.js
@@ -0,0 +1,13 @@
+
+if (process.argv.length < 3) {
+    console.log("Not a number");
+} else {
+    const input = process.argv[2];
+    const num = parseInt(input, 10);
+    
+    if (isNaN(num)) {
+        console.log("Not a number");
+    } else {
+        console.log(`My number: ${num}`);
+    }
+}
